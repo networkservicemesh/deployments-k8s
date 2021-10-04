a326dc339a7dada5ff514b0dde738de0260a95bf
Vladimir Popov <vladimir.popov@xored.com>
2021-10-04 12:44:58 +0700

    Add to-* scripts
    Signed-off-by: Vladimir Popov <vladimir.popov@xored.com>

---
+++ to-ref.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sedi() { sed -i "" "$@"; }
+else
+  sedi() { sed -i "$@"; }
+fi
+
+escape() {
+  echo "$1" | sed 's/\//\\\//g'
+}
+
+get_root() {
+  root="$(echo "$1" | sed 's/[^/]*$//g')"
+  root="$(echo "$(pwd)/${root}" | sed 's/[^/]*\//..\//g')"
+  root="$(echo "${root}$(pwd)" | sed 's/\/\//\//g')"
+  escape "${root}"
+}
+
+FILE_PATTERN="$(escape 'https://raw.githubusercontent.com/networkservicemesh/deployments-k8s/050a5817cb8104b907e2c241a7af38c08d3102d1/\1')"
+grep "$(pwd)" -rl examples/* | while IFS= read -r file; do
+  root="$(get_root "$file")"
+  sedi -E "s/${root//./\.}\/([^ ]*\.[a-z]+)/${FILE_PATTERN}/g" "${file}"
+done
+
+DIR_PATTERN="$(escape 'https://github.com/networkservicemesh/deployments-k8s/\1?ref=050a5817cb8104b907e2c241a7af38c08d3102d1')"
+grep "$(pwd)" -rl examples/* | while IFS= read -r file; do
+  root="$(get_root "$file")"
+  sedi -E "s/${root//./\.}\/([^ ]*)/${DIR_PATTERN}/g" "${file}"
+done
